c2aed7e13c4eb11df1bc5dfac92510380b372d65
0x2B <nhagan345@gmail.com>
2026-08-25 18:07:44 -0400

    feat: Load and create config file

--- anisync-cli/src/config.rs
+++ anisync-cli/src/config.rs
@@ -1,22 +1,122 @@
-use std::fmt::Result;
+use std::{
+    fs,
+    io::{self, ErrorKind, Write},
+    path::Path,
+};
 
-use serde::Deserialize;
 use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug)]
-struct Config {
-    auth: Auth
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Config {
+    pub myanimelist: Auth,
 }
 
-#[derive(Deserialize, Debug)]
-struct Auth {
-    client_id: String,
-    client_secret: String
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Auth {
+    pub client_id: String,
+    pub client_secret: String,
 }
 
 impl Config {
-    fn load() -> Result<Self> {
-        let config_dir = dirs::config_dir().context("Unable to locate config directory")?;
-        ()
+    pub fn load() -> Result<Self> {
+        let mut config_dir = dirs::config_dir().context("Unable to locate config directory")?;
+        config_dir.push("anisync");
+        let config_file_path = config_dir.join("config.toml");
+
+        Self::load_from(&config_dir, &config_file_path)
+    }
+
+    pub fn load_from(config_dir: &Path, config_file_path: &Path) -> Result<Self> {
+        match fs::read_to_string(config_file_path) {
+            Ok(str) => {
+                let config: Config = toml::from_str(&str).context("Failed to parse config.toml")?;
+                Ok(config)
+            }
+            Err(err) if err.kind() == ErrorKind::NotFound => {
+                println!("Config does not exist, let's make one!");
+                Self::setup_interactive(&config_dir, &config_file_path)
+            }
+            Err(err) => Err(err).context(format!(
+                "Failed to read config file at {:?}",
+                config_file_path
+            )),
+        }
+    }
+
+    pub fn setup_interactive(config_dir: &Path, config_file_path: &Path) -> Result<Self> {
+        let mut client_id = String::new();
+        print!("Enter Client ID ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut client_id)?;
+
+        let mut client_secret = String::new();
+        print!("Enter Client Secret ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut client_secret)?;
+
+        let config = Self {
+            myanimelist: Auth {
+                client_id: client_id.trim().to_string(),
+                client_secret: client_secret.trim().to_string(),
+            },
+        };
+
+        fs::create_dir_all(config_dir)?;
+
+        let config_text = toml::to_string_pretty(&config)?;
+
+        fs::write(config_file_path, config_text)?;
+
+        Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialize_deserialize() {
+        let config = Config {
+            myanimelist: Auth {
+                client_id: "client_id_123".to_string(),
+                client_secret: "client_secret_456".to_string(),
+            },
+        };
+        let serialized = toml::to_string(&config).unwrap();
+        assert!(serialized.contains("client_id_123"));
+        assert!(serialized.contains("client_secret_456"));
+
+        let deserialzed: Config = toml::from_str(serialized.as_str()).unwrap();
+
+        assert_eq!(
+            deserialzed.myanimelist.client_id,
+            "client_id_123".to_string()
+        );
+        assert_eq!(
+            deserialzed.myanimelist.client_secret,
+            "client_secret_456".to_string()
+        );
+    }
+
+    #[test]
+    fn test_load_from_existing_file() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let config_dir = temp_dir.path();
+        let config_file_path = config_dir.join("config.toml");
+
+        let toml_str = r#"
+            [myanimelist]
+            client_id = "mock_client"
+            client_secret = "mock_secret"
+        "#;
+
+        fs::write(&config_file_path, toml_str).unwrap();
+
+        let config = Config::load_from(config_dir, &config_file_path).unwrap();
+
+        assert_eq!(config.myanimelist.client_id, "mock_client".to_string());
+        assert_eq!(config.myanimelist.client_secret, "mock_secret".to_string());
     }
 }
--- anisync-cli/src/main.rs
+++ anisync-cli/src/main.rs
@@ -1,24 +1,29 @@
 use clap::Parser;
 use oauth2::{ClientId, RedirectUrl, basic::BasicClient};
 
-use crate::cli::{Cli, Commands};
+use crate::{
+    cli::{Cli, Commands},
+    config::Config,
+};
 
 mod cli;
 mod config;
 
-fn get_token() {
-    let client = BasicClient::new(
-        ClientId::new("asda".to_string())
-    )
-    .set_redirect_uri(RedirectUrl::new("".to_string()));
-}
+//fn get_token() {
+//let client = BasicClient::new(ClientId::new("asda".to_string()))
+//.set_redirect_uri(RedirectUrl::new("".to_string()));
+//}
 
 fn run_command(cli: &Cli) {
     match cli.command {
-        Commands::Auth => {}
+        Commands::Auth => match Config::load() {
+            Ok(config) => println!("{}", config.myanimelist.client_id),
+            Err(err) => println!("{}", err),
+        },
     }
 }
 
 fn main() {
     let cli = Cli::parse();
+    run_command(&cli);
 }
